5b158409dd205538b574fd95810de98fae814f01
Ankita <ankitakumawat111@gmail.com>
2024-07-04 11:23:15 +0530

    loading add

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import './home.css';
 
 const Home = () => {
     const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         axios.post('https://hoblist.com/api/movieList', {
@@ -16,9 +17,11 @@ const Home = () => {
             .then(response => {
                 const sortedMovies = response.data.result.sort((a, b) => b.voting - a.voting);
                 setMovies(sortedMovies);
+                setLoading(false);
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
+                setLoading(false);
             });
     }, []);
 
@@ -26,21 +29,25 @@ const Home = () => {
         <div>
             <NavBar />
             <h1>Home</h1>
-            <div className="card-container">
-                {movies.map(movie => (
-                    <div className="card" key={movie._id}>
-                        <img src={movie.poster} alt={movie.title} height={300} width={300} />
-                        <div className="card-content">
-                            <div className="card-title">{movie.title}</div>
-                            <div className="card-text">Director: {movie.director.join(', ')}</div>
-                            <div className="card-text">Starring: {movie.stars.join(', ')}</div>
-                            <div className="card-text">Language: {movie.language}</div>
-                            <div className="card-text">Release Date: {movie.releasedDate}</div>
-                            <div className="card-text">Votes: {movie.voting}</div>
+            {loading ? (
+                <div className="loading">Loading...</div>
+            ) : (
+                <div className="card-container">
+                    {movies.map(movie => (
+                        <div className="card" key={movie._id}>
+                            <img src={movie.poster} alt={movie.title} height={300} width={300} />
+                            <div className="card-content">
+                                <div className="card-title">{movie.title}</div>
+                                <div className="card-text">Director: {movie.director.join(', ')}</div>
+                                <div className="card-text">Starring: {movie.stars.join(', ')}</div>
+                                <div className="card-text">Language: {movie.language}</div>
+                                <div className="card-text">Release Date: {movie.releasedDate}</div>
+                                <div className="card-text">Votes: {movie.voting}</div>
+                            </div>
                         </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
